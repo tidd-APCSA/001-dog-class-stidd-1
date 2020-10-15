5a1d826cb102ebe95ad0fc4091384e1801e60084
Steven Tidd <stidd@aacps.org>
2020-10-15 14:10:56 +0000

    Completed the Dog class

---
+++ Dog.java
@@ -0,0 +1,39 @@
+public class Dog {
+  private String name;
+  private String breed;
+  private double weight;
+  private String color;
+  private boolean houseTrained;
+
+  public Dog(String theName, String breed, double weight, String color, boolean houseTrained){
+    name = theName;
+    this.breed = breed;
+    this.weight = weight;
+    this.color = color;
+    this.houseTrained = houseTrained;
+  }
+
+  // getter methods
+  public String getName(){
+    return this.name;
+  }
+
+  public String getBreed(){
+    return this.breed;
+  }
+
+
+  //@override
+  public String toString(){
+    return this.name + "🐶";
+  }
+
+
+
+
+
+
+
+
+
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
 public class Main {
   public static void main(String[] args) {
-    // test your code here by creating a three different dogs from the Dog class. Be sure you give each dog different characteristics. 
+    // test your code here by creating a three different dogs from the Dog class. Be sure you give each dog different characteristics.
+
+    Dog grimm = new Dog("Grimm", "chiwawa", 30, "gray", false);
+
+
+    System.out.println(grimm.getBreed());
+
+    Dog oconnor = new Dog("Ryan", "doodle", 80, "blonde", false);
+
+    System.out.println(oconnor.getBreed());
+
+
+
+
+
+
+
+
   }
 }
